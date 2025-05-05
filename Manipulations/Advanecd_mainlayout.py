fa8fbf4b4e66cd6abd0ef6d06e83f897a777bd36
Im HyeonJe <yusunju1357@gmail.com>
2025-05-05 19:05:21 +0900

    advanced controls화면 수정정

--- Manipulations/Advanecd_mainlayout.py
+++ Manipulations/Advanecd_mainlayout.py
@@ -2,7 +2,7 @@ from kivy.uix.screenmanager import Screen
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
-from kivy.graphics import Color, Rectangle
+from kivy.graphics import Color, Rectangle, RoundedRectangle
 
 
 class AdvancedMainLayout(Screen):
@@ -19,7 +19,7 @@ class AdvancedMainLayout(Screen):
         self.header = BoxLayout(size_hint_y=None, height=40, pos_hint = {'top': 1})
         with self.header.canvas.before:
             Color(177/255, 178/255, 1, 1)  # B1B2FF
-            self.header_bg = Rectangle(pos=self.header.pos, size=self.header.size, radius=[7, 7, 0, 0])
+            self.header_bg = RoundedRectangle(pos=self.header.pos, size=self.header.size)
         self.header.bind(pos=self.update_header_bg, size=self.update_header_bg)
 
         # 상단 바 텍스트
@@ -28,8 +28,30 @@ class AdvancedMainLayout(Screen):
 
         # 레이아웃 추가
         layout = FloatLayout()
+
+
+        # 왼쪽 박스
+        left_box = FloatLayout(size_hint=(0.2, 0.93), pos_hint={'x': 0, 'y': 0})
+        with left_box.canvas.before:
+            Color(210/255, 218/255, 1, 1)  # D2DAFF
+            left_box_bg = RoundedRectangle(pos=left_box.pos, size=left_box.size, radius=[0,10,0,0])
+        left_box.bind(pos=lambda *a: setattr(left_box_bg, 'pos', left_box.pos),
+                      size=lambda *a: setattr(left_box_bg, 'size', left_box.size))
+
+        # 오른쪽 박스
+        right_box = FloatLayout(size_hint=(0.2, 0.93), pos_hint={'right': 1, 'y': 0})
+        with right_box.canvas.before:
+            Color(210/255, 218/255, 1, 1)  # D2DAFF
+            right_box_bg = RoundedRectangle(pos=right_box.pos, size=right_box.size, radius=[10,0,0,0])
+        right_box.bind(pos=lambda *a: setattr(right_box_bg, 'pos', right_box.pos),
+                       size=lambda *a: setattr(right_box_bg, 'size', right_box.size))
+
+        # 레이아웃에 추가
+        layout.add_widget(left_box)
+        layout.add_widget(right_box)
         layout.add_widget(self.header)
 
+        
         self.add_widget(layout)
 
     def update_bg(self, *args):
